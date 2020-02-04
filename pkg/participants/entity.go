ba237b357aa98c0e2708c6c499b4c54cb919f538
L04DB4L4NC3R <angadsharma1016@gmail.com>
2020-02-05 00:51:42 +0530

    participant json fixed

--- pkg/participants/entity.go
+++ pkg/participants/entity.go
@@ -3,8 +3,8 @@ package participants
 type Participant struct {
 	EmailAddress       string `json:"emailAddress" bson:"emailAddress" validate:"nonzero"`
 	DeviceID           string `json:"deviceID,omitempty" bson:"deviceID,omitempty"`
-	PhoneNumber        string `json:"PhoneNumber" bson:"phoneNumber" validate:"min=7,max=22,nonzero"`
+	PhoneNumber        string `json:"phoneNumber" bson:"phoneNumber" validate:"min=7,max=22,nonzero"`
 	Name               string `json:"name" bson:"name" validate:"nonzero"`
-	RegistrationNumber string `json:"RegistrationNumber" bson:"RegistrationNumber" validate:"nonzero"`
+	RegistrationNumber string `json:"registrationNumber" bson:"RegistrationNumber" validate:"nonzero"`
 	EventName          string `json:"eventName" bson:"eventName" validate:"nonzero"`
 }
